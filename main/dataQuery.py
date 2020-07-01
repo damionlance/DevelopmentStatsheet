ec36c92569ffe98fb42fcba8afb6e3c9b4a62251
Andrew Lance <andrew@sidechainsecurity.com>
2020-07-01 11:39:03 -0700

    Implemented flask

--- main/dataQuery.py
+++ main/dataQuery.py
@@ -2,14 +2,25 @@ from nba_api.stats.endpoints import boxscoretraditionalv2
 from nba_api.stats.endpoints import playergamelog
 from nba_api.stats.static import players
 import pandas as pd
+from flask import Flask, jsonify, redirect, render_template, request
 
-def getPlayerPPG(fullName):
-    playerBox = getPlayerBox(fullName)
-    ppg = sum(playerBox.get('PTS'))/len(playerBox.index)
-    return ppg
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!!!"
+
+
+@app.route('/api/v1/ppg', methods=['GET'])
+def getPlayerPPG(lastname):
+	if request.method == 'GET':
+		lastname = request.args['lastname']
+	    playerBox = getPlayerBox(lastname)
+	    ppg = sum(playerBox.get('PTS'))/len(playerBox.index)
+	return ppg
 
 def getPlayerIDFromFullName(fullName):
-    return players.find_players_by_full_name(fullName)[0]['id']
+    return players.find_players_by_last_name(fullName)[0]['id']
 
 def getPlayerBox(fullName):
     playerID = getPlayerIDFromFullName(fullName)
@@ -20,4 +31,5 @@ def main():
     print(getPlayerPPG("Damian Lillard"))
 
 if __name__ == "__main__":
-    main()
+    #main()
+    app.run()
