6bcba7e836e6af182efb781616946754a06ad202
Damion <damion.lance@gmail.com>
2020-07-02 13:03:02 -0400

    first/last name fix; modified code structure

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from nba_api.stats.endpoints import boxscoretraditionalv2
-from nba_api.stats.endpoints import playergamelog
-from nba_api.stats.static import players
-import pandas as pd
+from main.dataQuery import *
 from flask import Flask, jsonify, redirect, render_template, request
 
 app = Flask(__name__)
@@ -10,27 +7,19 @@ app = Flask(__name__)
 def hello():
     return "Hello World!!!"
 
-
 @app.route('/api/v1/ppg', methods=['GET'])
-def getPlayerPPG():
+def getPlayerPPG(firstName=None, lastName=None):
     if request.method == 'GET':
-        lastname = request.args['lastname']
-        playerBox = getPlayerBox(lastname)
+        if 'lastName' in request.args and 'firstName' in request.args:
+            lastName = request.args['lastName']
+            firstName = request.args['firstName']
+        fullName = firstName + " " + lastName
+        playerBox = getPlayerBox(fullName)
         ppg = sum(playerBox.get('PTS'))/len(playerBox.index)
+    else:
+        ppg = 0
     return str(ppg)
 
-def getPlayerIDFromFullName(fullName):
-    return players.find_players_by_last_name(fullName)[0]['id']
-
-def getPlayerBox(fullName):
-    playerID = getPlayerIDFromFullName(fullName)
-    boxScores = playergamelog.PlayerGameLog(season="2019", player_id=playerID, season_type_all_star="Regular Season").player_game_log.get_data_frame()
-    return boxScores
-
-def main():
-    print(getPlayerPPG("Damian Lillard"))
-
 if __name__ == "__main__":
-    #main()
     app.debug=True
     app.run()
